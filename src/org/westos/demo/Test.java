25c7584c04674e34334e04d4f8b858e0c8efd826
dylan <2969386452@qq.com>
2019-12-25 20:24:53 +0800

    第二次提交

---
+++ src/org/westos/demo/Test.java
@@ -0,0 +1,11 @@
+package org.westos.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("git的第一个项目上传！"+i);
+        }
+        System.out.println("github");
+    }
+
+}
